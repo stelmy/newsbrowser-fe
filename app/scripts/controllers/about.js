ee96b6c195b5bf6cddae32d47f375be4e020716d
stelmy <stelmy@o2.pl>
2018-02-21 06:31:45 +0100

    Yeoman generated application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsbrowserFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newsbrowserFeApp
+ */
+angular.module('newsbrowserFeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newsbrowserFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newsbrowserFeApp
+ */
+angular.module('newsbrowserFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

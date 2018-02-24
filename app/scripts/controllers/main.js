895c1b18f26598525de9a0b81aaea5f47a08ade4
stelmy <stelmy@o2.pl>
2018-02-24 14:02:17 +0100

    Wyświetlanie listy artykułów.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,15 +3,15 @@
 /**
  * @ngdoc function
  * @name newsbrowserFeApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the newsbrowserFeApp
+ * @description # MainCtrl Controller of the newsbrowserFeApp
  */
-angular.module('newsbrowserFeApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('newsbrowserFeApp').controller(
+    'MainCtrl',
+    function($http, $scope) {
+      this.awesomeThings = [ 'HTML5 Boilerplate', 'AngularJS', 'Karma' ];
+
+      $http.get('http://localhost:8080/news/pl/technology').then(
+          function(response) {
+            $scope.articles = response.data.articles;
+          });
+    });
